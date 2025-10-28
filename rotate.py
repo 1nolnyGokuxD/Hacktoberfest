002356f3d2894cbed5e0c355325a42fea1d7214e
1nolnyGokuxD <Tagru172@gmail.com>
2025-10-28 18:05:24 +0530

    Create rotate.py

---
+++ rotate.py
@@ -0,0 +1,13 @@
+Class Solution:
+def rotate(self, matrix: List[List[int]]) >
+None:
+1-0
+rlen(matrix) -1
+while lr:
+matrix[1], matrix[r] matrix[r], matrix [1]
+1-1
+1
+#transpose
+for i in range(len(matrix)):
+for j in range(1):
+matrix[i][j], matrix[j][i]matrix[j] [i], matrix[i][j]
